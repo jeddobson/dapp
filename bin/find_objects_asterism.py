ed9e263894bfa55fc1c452cbf1f574025af425cd
James Dobson <james.e.dobson@dartmouth.edu>
2018-02-19 10:32:12 -0500

    removed space

--- bin/find_objects_asterism.py
+++ bin/find_objects_asterism.py
@@ -11,6 +11,18 @@ object = sys.argv[1]
 
 # load and process pattern images
 asterism_gray = cv2.imread('share/asterism.jpg',0)
+inverted_asterism_gray = cv2.imread('share/inverted_asterism.jpg',0)
+
+def find_inverted_asterism(target,output):
+    (tH, tW) = inverted_asterism_gray.shape[:2]
+    res = cv2.matchTemplate(target,inverted_asterism_gray,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.60
+    locations = np.where(res >= threshold)
+    count=0
+    for pt in zip(*locations[::-1]):
+        count = count + 1
+        #cv2.rectangle(output, pt, (pt[0] + tW, pt[1] + tH), (0,0,255), 2)
+    return(count)
 
 def find_asterism(target,output):
     (tH, tW) = asterism_gray.shape[:2]
@@ -33,10 +45,11 @@ found_objects=list()
 for image_tif in glob.glob(image_dir + '*.TIF'):
     objects = 0
     gray_image = cv2.imread(image_tif, 0)
-    asterism_c = find_asterism(gray_image,gray_image) 
 
-    found_objects.append([image_tif,asterism_c])
+    asterism_c = find_asterism(gray_image,gray_image) 
+    inverted_asterism_c = find_inverted_asterism(gray_image,gray_image) 
+
+    found_objects.append([image_tif,asterism_c,inverted_asterism_c])
 
 output_pickle=open(output_dir + '/asterism_count.pkl','wb')
 pickle.dump(found_objects,output_pickle)
-
